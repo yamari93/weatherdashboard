919d4b707d9149be3295862eb4bdf0d63aeddd48
yamari <yamarigonzalez25@gmail.com>
2023-04-15 23:47:06 -0500

    added last search results from local storage to display

---
+++ ChallegeMaterials/Assets/README.md
@@ -0,0 +1,51 @@
+# Weather Dashboard
+
+# Description
+
+An application that is able to search weather from different locations selected by the user. Search would show the date, description of weather, temperature, and humidity.
+
+# Table of Contents
+
+- [Link](#link)
+- [Technologies](#technologies)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+# Link
+
+Visit the deployed application:  
+[https://fifthoctave.herokuapp.com/](https://fifthoctave.herokuapp.com/)
+
+# Technologies
+
+- React
+- GraphQL
+- MongoDB & Mongoose
+- Stripe
+- JSX
+- Tailwind CSS
+- CSS
+- JavaScript
+- HTML
+
+# Usage
+
+Search for weather by city. See
+
+# Contributing
+
+- Yamari Gonzalez
+
+# Questions
+
+For further information:
+
+- GitHub:
+
+- [Yamari's GitHub](https://github.com/yamari93)
+
+# Screenshot
+
+![screenshot of Weather Dashboard](/client/public/images/screenshot.png)
--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,21 @@ let requestUrl = [];
 //Functions
 function init() {
   // grab last search results from local storage & display on page
+  if (searchCities.length > 0) {
+    let maximumLength = searchedCities.length >= 5 ? 5 : searchedCities.length;
+    let count = 0;
+    for (let i = searchedCities.length - 1; i >= 0; i--) {
+      let cityHistoryButtons = document.createElement("button");
+      cityHistoryButtons.setAttribute("data-city", searchedCities[i]);
+      cityHistoryButtons.innerHTML = searchedCities[i];
+      cityHistoryButtons.classList.add("w-full", "border-4", "border-blue", "rounded-lg", "my-3", "text-center");
+      searchHistoryButtons.append(cityHistoryButtons);
+      count++;
+      if (count === maximumLength) {
+        break;
+      }
+    }
+  }
 }
 
 function search() {
@@ -28,11 +43,41 @@ function search() {
 
 function renderItems(city, data) {
   document.querySelector("#currentCity").innerHTML = city;
+
   document.querySelector("#day1Date").innerHTML = data.list[0].dt_txt;
   document.querySelector("#day1Description").innerHTML = data.list[0].weather[0].description;
   let kTemp = data.list[0].main.temp;
   let fTemp = ((kTemp - 273.15) * 9) / 5 + 32;
   document.querySelector("#day1Temp").innerHTML = fTemp;
+  document.querySelector("#day1Humidity").innerHTML = data.list[0].main.humidity;
+
+  document.querySelector("#day2Date").innerHTML = data.list[1].dt_txt;
+  document.querySelector("#day2Description").innerHTML = data.list[1].weather[0].description;
+  let kTemp2 = data.list[1].main.temp;
+  let fTemp2 = ((kTemp2 - 273.15) * 9) / 5 + 32;
+  document.querySelector("#day2Temp").innerHTML = fTemp2;
+  document.querySelector("#day2Humidity").innerHTML = data.list[1].main.humidity;
+
+  document.querySelector("#day3Date").innerHTML = data.list[2].dt_txt;
+  document.querySelector("#day3Description").innerHTML = data.list[2].weather[0].description;
+  let kTemp3 = data.list[2].main.temp;
+  let fTemp3 = ((kTemp3 - 273.15) * 9) / 5 + 32;
+  document.querySelector("#day3Temp").innerHTML = fTemp3;
+  document.querySelector("#day3Humidity").innerHTML = data.list[2].main.humidity;
+
+  document.querySelector("#day4Date").innerHTML = data.list[3].dt_txt;
+  document.querySelector("#day4Description").innerHTML = data.list[3].weather[0].description;
+  let kTemp4 = data.list[3].main.temp;
+  let fTemp4 = ((kTemp4 - 273.15) * 9) / 5 + 32;
+  document.querySelector("#day4Temp").innerHTML = fTemp4;
+  document.querySelector("#day4Humidity").innerHTML = data.list[3].main.humidity;
+
+  document.querySelector("#day5Date").innerHTML = data.list[4].dt_txt;
+  document.querySelector("#day5Description").innerHTML = data.list[4].weather[0].description;
+  let kTemp5 = data.list[4].main.temp;
+  let fTemp5 = ((kTemp5 - 273.15) * 9) / 5 + 32;
+  document.querySelector("#day5Temp").innerHTML = fTemp5;
+  document.querySelector("#day5Humidity").innerHTML = data.list[4].main.humidity;
 }
 
 //Function calls
